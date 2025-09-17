12ba44e5937db96232b4bbb6262fe1bb0366f6f9
CapashenRocks <88005278+CapashenRocks@users.noreply.github.com>
2025-09-17 11:05:42 -0400

    Updated with location and logout announcement

--- Announce Login.lua
+++ Announce Login.lua
@@ -1,5 +1,7 @@
---Lua Script: Player Login Announcement Script
+--Lua Script: Player Login & Logout Announcement Script
 --Coded by: CapashenRocks (Github)/Capashen22 (Discord)
+--V2- Added login location and a logout announcement.
+
 
 local CLASS_COLORS = {
     ["Warrior"]     = "|cffC79C6EWarrior|r",
@@ -17,15 +19,11 @@ local CLASS_COLORS = {
     ["Evoker"]      = "|cff33937FEvoker|r"
 }
 
---For reference if purple is preferred instead of green, replace below: cff00ff00= green, cffaa00ff= purple
-
 local raceNames = { 
     [1] = "|cff00ff00Human|r", [2] = "|cff00ff00Orc|r", [3] = "|cff00ff00Dwarf|r", [4] = "|cff00ff00Night Elf|r",
     [5] = "|cff00ff00Undead|r", [6] = "|cff00ff00Tauren|r", [7] = "|cff00ff00Gnome|r", [8] = "|cff00ff00Troll|r", [9]= "|cff00ff00Goblin|r",[10] = "|cff00ff00Blood Elf|r", [11] = "|cff00ff00Draenei|r", [22] = "|cff00ff00Worgen|r", [25]= "|cff00ff00Pandaren|r", [26]= "|cff00ff00Pandaren|r", [27]= "|cff00ff00Nightbourne|r", [28]= "|cff00ff00Highmountain Tauren|r", [29]= "|cff00ff00Void Elf|r", [30]= "|cff00ff00Lightforged Draenei|r", [31]= "|cff00ff00Zandalari Troll|r", [32]= "|cff00ff00Kul Tiran|r", [34]= "|cff00ff00Dark Iron Dwarf|r", [35]= "|cff00ff00Vulpera|r", [36]= "|cff00ff00Mag'har Orc|r", [37]= "|cff00ff00Mechagnome|r", [52]= "|cff00ff00Dracthyr|r", [70]= "|cff00ff00Dracthyr|r", [84]= "|cff00ff00Earthen Dwarf|r", [85]= "|cff00ff00Earthen Dwarf|r", 
-
 }
 
---Originally coded names to match faction color, to change change that back update the below: Horde to cffFF0000, Alliance to cff0070
 local function ColorName(player)
     if player:GetTeam() == 0 then -- Alliance
         return "|cff00ff00"..player:GetName().."|r"
@@ -47,14 +45,35 @@ local function GetClassColored(player)
     return CLASS_COLORS[class] 
 end
 
-local function OnLogin(event, player)
-    local race = raceNames[player:GetRace()]
-    local coloredName = ColorName(player)
-    local class = GetClassColored(player)
-    local faction = ColorFaction(player)
-  
-   SendWorldMessage(string.format("Local spies report that a %s %s named %s has logged in! Intelligence indicates they are loyal to the %s.", race, class, coloredName, faction))
+local function GetPlayerArea(player)
+    local areaId = player:GetAreaId()
+    local areaName = GetAreaName and GetAreaName(areaId, 0) or "Deep in the Wilderness" -- 0 = default locale
+    return "|cff00ff00" .. areaName .. "|r"
+end
 
+local function OnLogin(event, player)
+    local race = raceNames[player:GetRace()] or "Unknown Race"
+    local coloredName = ColorName(player)
+    local class = GetClassColored(player) or "Unknown Class"
+    local faction = ColorFaction(player)
+    local area = GetPlayerArea(player)
+    SendWorldMessage(string.format(
+        "Local spies report that a %s %s named %s has logged in at %s! Intelligence indicates they are loyal to the %s.",
+        race, class, coloredName, area, faction
+    ))
+end
+
+local function OnLogout(event, player)
+    local race = raceNames[player:GetRace()] or "Unknown Race"
+    local coloredName = ColorName(player)
+    local class = GetClassColored(player) or "Unknown Class"
+    local faction = ColorFaction(player)
+    local area = GetPlayerArea(player)
+    SendWorldMessage(string.format(
+        "Local spies report that a %s %s named %s has logged out from %s! Agents confirm their allegiance to the %s.",
+        race, class, coloredName, area, faction
+    ))
 end
 
 RegisterPlayerEvent(3, OnLogin)
+RegisterPlayerEvent(4, OnLogout)
